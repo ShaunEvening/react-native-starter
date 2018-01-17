23821435e2d24396dd51e3c2c79d3d7ff2e48c7b
Chris DePaul <chris.depaul@rangle.io>
2018-01-17 17:43:34 -0500

    Config Scripts make changes to specified files. App.json contains Template Text. package.json contains Template Text and npm scripts that call the config scripts

---
+++ config-scripts/util.js
@@ -0,0 +1,16 @@
+/**
+ * Utility function for modifying files
+ */
+const fs = require('fs');
+
+module.exports.modifyFiles = function modifyFiles(files, replacements) {
+  files.forEach((file) => {
+    let fileContentModified = fs.readFileSync(file, 'utf8');
+
+    replacements.forEach((v) => {
+      fileContentModified = fileContentModified.replace(v.regexp, v.replacement);
+    })
+
+    fs.writeFileSync(file, fileContentModified, 'utf8');
+  });
+};
